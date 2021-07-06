cc3a97dcd2bbf4210f95a9a605009b400b2becd9
nazkova <nazariy.kovalyk@gmail.com>
2021-07-06 14:42:37 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+document.querySelector('body');
+
+
+// const background = prompt("Please enter a color of this page : (red, black, white)");
+// if (background === "red") {
+//     document.body.style.background = "red";
+// } else if (background === "black") {
+//     document.body.style.background = "black";
+// } else if (background === "white") {
+//     document.body.style.background = "white";
+// }
+
+
+
+
+// const font = prompt("Please enter a type of font : (oblique, italic, normal)");
+// if (font === "oblique") {
+//     document.body.style.fontStyle = "oblique";
+// }
+// else if (font === "italic") {
+//     document.body.style.fontStyle = "italic";
+// }
+// else if (font === "normal") {
+//     document.body.style.fontStyle = "normal";
+// }
+
+
+
+
+// const centrik = prompt("Do you want to move h1 to the center? (yes or no)");
+// if (centrik === "yes") {
+//     document.getElementById("demo").style.textAlign = "center";
+// } else if (centrik === "no") {
+//     document.getElementById("demo").style.textAlign = "left";
+// }
+
+
+
+
+
+// const bgColor = prompt('Enter a color for paragraph')
+// document.getElementById("par").style.backgroundColor = bgColor;
+
+// let textColor = prompt('Enter color of the text');
+//       for(let i = 0; i < par.children.length; i++){
+//       par.children[i].style.color= textColor;
+//    }
+
+
+
+// const colSize = prompt('Enter color of div element');
+// let block = document.body.children[2];
+// block.style.color = colSize;
+
+// const colSize1 = prompt('Enter size of div element');
+// block.style.fontSize = colSize1;
+
+// const colSize2 = prompt('Enter thickness of div element');
+// block.style.fontStyle = colSize2;
+
+
+
+
+// const listTyp = prompt('Change list style type');
+// const lis = document.body.querySelector('ul');
+// lis.style.listStyleType = listTyp;
+
+
+
+const repSite = prompt('Enter your net page');
+let text = document.getElementById("href1");
+text.replace("google", repSite);
